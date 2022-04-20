914473a286f53ad448922e33f24232262bb25c58
Adi Dzocaj <adidzocaj@gmail.com>
2022-04-21 00:08:45 +0200

    Created the navbar and routes that point to the homepage, people and films

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import People from "./pages/People";
+import Films from "./pages/Films";
 
-function App() {
+const App = () => {
   return (
     <div>
-      <p>HELLO</p>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/films" element={<Films />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <div className="navbar-container">
+      <Link to={"/"}>
+        {" "}
+        <h1 className="navbar-header-link">SWpedia</h1>
+      </Link>
+      <div>
+        <Link className="navbar-link" to={"/people"}>
+          People
+        </Link>
+        <Link className="navbar-link" to={"/films"}>
+          Films
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
